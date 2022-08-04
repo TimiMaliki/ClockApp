1db6edd8eac1e48f7166ab99b09cb842879c7a45
user.name <malikitimi@gmail.com>
2022-08-04 22:42:01 +0100

    code war

---
+++ js progress/Variables/src/var.js
@@ -0,0 +1,15 @@
+// i'm currently starting my first variable and declaring it
+
+var money = 200;
+
+// now i'm learning the difference between  a local and global
+
+var myVar = "Global" // declares a global
+
+// now declaring a local variable
+ function checkscope(){
+    var myVar ="Local" // declare a Local
+    document.write(myVar)
+ };
+
+  
